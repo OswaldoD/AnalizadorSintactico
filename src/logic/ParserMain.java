39f5814bc8de0c696fba6204815872f2825ebdd1
OswaldoD <dreammicro7@hotmail.com>
2015-09-29 01:06:33 -0600

    clase parser

---
+++ src/logic/ParserMain.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.util.Stack;
+
+public class ParserMain {
+
+	private Stack<String> Pila = new Stack<String>();
+	private Token token;
+	
+	public ParserMain(){
+		
+	}
+}
